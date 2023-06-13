e2062d51a5d9519676ee9412be5d0a93da332bc0
TrinhDuy25 <92090085+TrinhDuy25@users.noreply.github.com>
2023-06-13 16:23:26 +0700

    update border config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,12 +23,12 @@ module.exports = {
     },
     borderWidth: {
       DEFAULT: '1px',
-      '0': '0',
-      '2': '2px',
-      '3': '3px',
-      '4': '4px',
-      '6': '6px',
-      '8': '8px',
+      0: '0',
+      2: '2px',
+      3: '3px',
+      4: '4px',
+      6: '6px',
+      8: '8px',
     },
     extend: {
       colors: {
