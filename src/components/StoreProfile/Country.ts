debe0d9ae3dcbaa49cc69f1771ca466b270f927f
TrinhDuy25 <92090085+TrinhDuy25@users.noreply.github.com>
2023-06-13 16:00:26 +0700

    update about your salon, style progess bar

---
+++ src/components/StoreProfile/Country.ts
@@ -0,0 +1,24 @@
+import type { CountryType } from '@/types/country';
+
+const countries: readonly CountryType[] = [
+  { code: 'AD', label: 'Andorra', phone: '376' },
+  {
+    code: 'AE',
+    label: 'United Arab Emirates',
+    phone: '971',
+  },
+  { code: 'AF', label: 'Afghanistan', phone: '93' },
+  {
+    code: 'AG',
+    label: 'Antigua and Barbuda',
+    phone: '1-268',
+  },
+  { code: 'AI', label: 'Anguilla', phone: '1-264' },
+  { code: 'AL', label: 'Albania', phone: '355' },
+  { code: 'AM', label: 'Armenia', phone: '374' },
+  { code: 'AO', label: 'Angola', phone: '244' },
+  { code: 'AQ', label: 'Antarctica', phone: '672' },
+  { code: 'AR', label: 'Argentina', phone: '54' },
+];
+
+export default countries;
