28a37159c42f3a40e3e46c753a918170e254d2fa
binhnt <thanhbinh.bid@enrichco.us>
2023-06-13 15:48:09 +0700

    finish UI Working Hours
    finish UI Working Hours

---
+++ src/components/StoreProfile/EditBusinessHours.tsx
@@ -0,0 +1,255 @@
+import {
+  FormControlLabel,
+  Switch,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  FormHelperText,
+  Select,
+  Grid,
+  Divider,
+  Button,
+} from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import type { NextPage } from "next";
+import Image from "next/image";
+import { useState } from "react";
+
+const EditBusinessHours: NextPage = () => {
+  const [startHour, setStartHour] = useState("");
+  const [endHour, setEndHour] = useState("");
+  const [forms, setForms] = useState([{ id: 0 }]);
+  const addForm = () => {
+    const newFormId = forms.length + 1;
+    setForms([...forms, { id: newFormId }]);
+  };
+  const removeForm = (formId: number) => {
+    setForms(forms.filter((form) => form.id !== formId));
+  };
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-mango-gray-light-2 bg-cover bg-center bg-no-repeat ">
+      <div className="flex min-h-[70%] w-[35%] flex-col items-center justify-between gap-2 rounded-2xl bg-white p-6 py-5">
+        {/* <div className="absolute left-[0px] top-[0px]  h-4 w-full rounded-t-xl bg-primary-states-hover" />
+        <div className="absolute left-[0px] top-[0px] h-4 w-60 rounded-t-xl [background:linear-gradient(90deg,_#80dfeb_68.23%,_rgba(255,_255,_255,_0.52)_99.99%,_rgba(0,_240,_255,_0))]" /> */}
+        <div className=" mt-12 w-[90%]">
+          <div className="flex items-center justify-center ">
+            <img
+              className="cursor-pointer text-3xl"
+              src="/closefilled.svg"
+              alt=""
+            />
+
+            <p className="mx-auto text-[32px] font-semibold text-center text-text-title">
+              Edit business hours
+            </p>
+          </div>
+
+          <div className="w-full flex flex-col gap-[12px] text-text-primary">
+            <div className="flex flex-row items-center justify-start  text-center mt-8">
+              <div>
+                <Switch className="p-3" />
+              </div>
+              <div className="w-[30%] leading-[133.4%] font-semibold">
+                Every Monday
+              </div>
+              <div className="leading-[140%] text-text-secondary">12h 0min</div>
+            </div>
+            <Grid className="items-center" container spacing={4}>
+              <Grid item xs={5.5}>
+                <label className="leading-[140%] font-semibold">Start</label>
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel color="primary" />
+                  <Select
+                    color="primary"
+                    defaultValue="09:00 AM"
+                    size="medium"
+                    onChange={(e) => setStartHour(e.target.value)}
+                  >
+                    <MenuItem value="05:00 AM">05:00 AM</MenuItem>
+                    <MenuItem value="06:00 AM">06:00 AM</MenuItem>
+                    <MenuItem value="07:00 AM">07:00 AM</MenuItem>
+                    <MenuItem value="08:00 AM">08:00 AM</MenuItem>
+                    <MenuItem value="09:00 AM">09:00 AM</MenuItem>
+                    <MenuItem value="10:00 AM">10:00 AM</MenuItem>
+                    <MenuItem value="11:00 AM">11:00 AM</MenuItem>
+                    <MenuItem value="12:00 AM">12:00 AM</MenuItem>
+                    <MenuItem value="01:00 PM">01:00 PM</MenuItem>
+                    <MenuItem value="02:00 PM">02:00 PM</MenuItem>
+                    <MenuItem value="03:00 PM">03:00 PM</MenuItem>
+                    <MenuItem value="04:00 PM">04:00 PM</MenuItem>
+                    <MenuItem value="05:00 PM">05:00 PM</MenuItem>
+                    <MenuItem value="06:00 PM">06:00 PM</MenuItem>
+                    <MenuItem value="07:00 PM">07:00 PM</MenuItem>
+                    <MenuItem value="08:00 PM">08:00 PM</MenuItem>
+                    <MenuItem value="09:00 PM">09:00 PM</MenuItem>
+                    <MenuItem value="10:00 PM">10:00 PM</MenuItem>
+                    <MenuItem value="11:00 PM">11:00 PM</MenuItem>
+                  </Select>
+                  <FormHelperText />
+                </FormControl>
+              </Grid>
+              <Grid item xs={1}>
+                <div className="box-border h-px w-[70] border-t-[2px] border-solid border-line-light p-[5px] mt-8" />
+              </Grid>
+              <Grid item xs={5.5}>
+                <label className="leading-[140%] font-semibold">End</label>
+                <FormControl fullWidth variant="outlined">
+                  <InputLabel color="primary" />
+                  <Select
+                    color="primary"
+                    defaultValue="09:00 PM"
+                    size="medium"
+                    onChange={(e) => setEndHour(e.target.value)}
+                  >
+                    <MenuItem value="05:00 AM">05:00 AM</MenuItem>
+                    <MenuItem value="06:00 AM">06:00 AM</MenuItem>
+                    <MenuItem value="07:00 AM">07:00 AM</MenuItem>
+                    <MenuItem value="08:00 AM">08:00 AM</MenuItem>
+                    <MenuItem value="09:00 AM">09:00 AM</MenuItem>
+                    <MenuItem value="10:00 AM">10:00 AM</MenuItem>
+                    <MenuItem value="11:00 AM">11:00 AM</MenuItem>
+                    <MenuItem value="12:00 AM">12:00 AM</MenuItem>
+                    <MenuItem value="01:00 PM">01:00 PM</MenuItem>
+                    <MenuItem value="02:00 PM">02:00 PM</MenuItem>
+                    <MenuItem value="03:00 PM">03:00 PM</MenuItem>
+                    <MenuItem value="04:00 PM">04:00 PM</MenuItem>
+                    <MenuItem value="05:00 PM">05:00 PM</MenuItem>
+                    <MenuItem value="06:00 PM">06:00 PM</MenuItem>
+                    <MenuItem value="07:00 PM">07:00 PM</MenuItem>
+                    <MenuItem value="08:00 PM">08:00 PM</MenuItem>
+                    <MenuItem value="09:00 PM">09:00 PM</MenuItem>
+                    <MenuItem value="10:00 PM">10:00 PM</MenuItem>
+                    <MenuItem value="11:00 PM">11:00 PM</MenuItem>
+                  </Select>
+                  <FormHelperText />
+                </FormControl>
+              </Grid>
+            </Grid>
+            {forms.length > 1 ? (
+              <>
+                <div className="box-border h-px w-[70] border-t-[2px] border-solid border-line-light p-[5px] mt-8" />
+                <label className="leading-[140%] font-semibold">
+                  Break Time
+                </label>
+              </>
+            ) : (
+              ""
+            )}
+            {forms.length > 1 ? (
+              <>
+                {forms.map((form) => (
+                  <>
+                    {form.id > 0 ? (
+                      <div key={form.id}>
+                        <Grid className="items-center" container spacing={3}>
+                          <Grid item xs={5}>
+                            <FormControl fullWidth variant="outlined">
+                              <InputLabel color="primary" />
+                              <Select
+                                color="primary"
+                                defaultValue="09:00 AM"
+                                size="medium"
+                                onChange={(e) => setStartHour(e.target.value)}
+                              >
+                                <MenuItem value="05:00 AM">05:00 AM</MenuItem>
+                                <MenuItem value="06:00 AM">06:00 AM</MenuItem>
+                                <MenuItem value="07:00 AM">07:00 AM</MenuItem>
+                                <MenuItem value="08:00 AM">08:00 AM</MenuItem>
+                                <MenuItem value="09:00 AM">09:00 AM</MenuItem>
+                                <MenuItem value="10:00 AM">10:00 AM</MenuItem>
+                                <MenuItem value="11:00 AM">11:00 AM</MenuItem>
+                                <MenuItem value="12:00 AM">12:00 AM</MenuItem>
+                                <MenuItem value="01:00 PM">01:00 PM</MenuItem>
+                                <MenuItem value="02:00 PM">02:00 PM</MenuItem>
+                                <MenuItem value="03:00 PM">03:00 PM</MenuItem>
+                                <MenuItem value="04:00 PM">04:00 PM</MenuItem>
+                                <MenuItem value="05:00 PM">05:00 PM</MenuItem>
+                                <MenuItem value="06:00 PM">06:00 PM</MenuItem>
+                                <MenuItem value="07:00 PM">07:00 PM</MenuItem>
+                                <MenuItem value="08:00 PM">08:00 PM</MenuItem>
+                                <MenuItem value="09:00 PM">09:00 PM</MenuItem>
+                                <MenuItem value="10:00 PM">10:00 PM</MenuItem>
+                                <MenuItem value="11:00 PM">11:00 PM</MenuItem>
+                              </Select>
+                              <FormHelperText />
+                            </FormControl>
+                          </Grid>
+                          <Grid item xs={1}>
+                            <div className="box-border h-px w-[70] border-t-[2px] border-solid border-line-light p-[5px]" />
+                          </Grid>
+                          <Grid item xs={5}>
+                            <FormControl fullWidth variant="outlined">
+                              <InputLabel color="primary" />
+                              <Select
+                                color="primary"
+                                defaultValue="09:00 PM"
+                                size="medium"
+                                onChange={(e) => setEndHour(e.target.value)}
+                              >
+                                <MenuItem value="05:00 AM">05:00 AM</MenuItem>
+                                <MenuItem value="06:00 AM">06:00 AM</MenuItem>
+                                <MenuItem value="07:00 AM">07:00 AM</MenuItem>
+                                <MenuItem value="08:00 AM">08:00 AM</MenuItem>
+                                <MenuItem value="09:00 AM">09:00 AM</MenuItem>
+                                <MenuItem value="10:00 AM">10:00 AM</MenuItem>
+                                <MenuItem value="11:00 AM">11:00 AM</MenuItem>
+                                <MenuItem value="12:00 AM">12:00 AM</MenuItem>
+                                <MenuItem value="01:00 PM">01:00 PM</MenuItem>
+                                <MenuItem value="02:00 PM">02:00 PM</MenuItem>
+                                <MenuItem value="03:00 PM">03:00 PM</MenuItem>
+                                <MenuItem value="04:00 PM">04:00 PM</MenuItem>
+                                <MenuItem value="05:00 PM">05:00 PM</MenuItem>
+                                <MenuItem value="06:00 PM">06:00 PM</MenuItem>
+                                <MenuItem value="07:00 PM">07:00 PM</MenuItem>
+                                <MenuItem value="08:00 PM">08:00 PM</MenuItem>
+                                <MenuItem value="09:00 PM">09:00 PM</MenuItem>
+                                <MenuItem value="10:00 PM">10:00 PM</MenuItem>
+                                <MenuItem value="11:00 PM">11:00 PM</MenuItem>
+                              </Select>
+                              <FormHelperText />
+                            </FormControl>
+                          </Grid>
+                          <Grid item xs={1}>
+                            <Button
+                              className="justify-center items-center"
+                              variant="text"
+                              startIcon={
+                                <DeleteOutlineIcon sx={{ color: "#F28500" }} />
+                              }
+                              onClick={() => removeForm(form.id)}
+                            ></Button>
+                          </Grid>
+                        </Grid>
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                  </>
+                ))}
+              </>
+            ) : (
+              ""
+            )}
+
+            <Button
+              className="justify-start text-primary-main font-semibold"
+              variant="text"
+              startIcon={<AddIcon sx={{ color: "#00bdd6" }} />}
+              onClick={addForm}
+            >
+              Add Break
+            </Button>
+            <button
+              type="button"
+              className="mt-8 box-border flex w-[100%] flex-col items-center justify-center overflow-hidden rounded bg-primary-main px-[22px] py-[13px] text-[18px] font-bold text-primary-contrast shadow-[0px_1px_5px_rgba(0,_0,_0,_0.12),_0px_2px_2px_rgba(0,_0,_0,_0.14),_0px_3px_1px_-2px_rgba(0,_0,_0,_0.2)]"
+            >
+              SAVE
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default EditBusinessHours;
--- src/components/StoreProfile/StoreWorkingHoursSetup.tsx
+++ src/components/StoreProfile/StoreWorkingHoursSetup.tsx
@@ -1,21 +1,22 @@
-import { Switch } from '@mui/material';
-import type { NextPage } from 'next';
+import { Switch } from "@mui/material";
+import type { NextPage } from "next";
+import Image from "next/image";
 
 const StoreWorkingHoursSetup: NextPage = () => {
   const listData = [
-    { id: 1, day: 'Sunday', workTime: 'Closed' },
-    { id: 2, day: 'Monday', workTime: '9:00 AM - 9:00 PM' },
-    { id: 3, day: 'Tuesday', workTime: '9:00 AM - 9:00 PM' },
-    { id: 4, day: 'Wednesday', workTime: '9:00 AM - 9:00 PM' },
-    { id: 5, day: 'Thursday', workTime: '9:00 AM - 9:00 PM' },
-    { id: 6, day: 'Friday', workTime: '9:00 AM - 9:00 PM' },
-    { id: 7, day: 'Saturday', workTime: 'Closed' },
+    { id: 1, day: "Sunday", workTime: "Closed" },
+    { id: 2, day: "Monday", workTime: "9:00 AM - 9:00 PM" },
+    { id: 3, day: "Tuesday", workTime: "9:00 AM - 9:00 PM" },
+    { id: 4, day: "Wednesday", workTime: "9:00 AM - 9:00 PM" },
+    { id: 5, day: "Thursday", workTime: "9:00 AM - 9:00 PM" },
+    { id: 6, day: "Friday", workTime: "9:00 AM - 9:00 PM" },
+    { id: 7, day: "Saturday", workTime: "Closed" },
   ];
   return (
-    <div className="relative h-[1080px] w-full bg-background-paper-elevation-1 text-center text-base text-text-secondary">
-      <div className="absolute left-[676px] top-[calc(50%_-_400px)] h-[716px] w-[568px] rounded-2xl bg-primary-contrast">
-        <div className="absolute left-[0px] top-[0px]  h-4 w-full rounded-t-xl bg-primary-states-hover" />
-        <div className="absolute left-[0px] top-[0px] h-4 w-60 rounded-t-xl [background:linear-gradient(90deg,_#80dfeb_68.23%,_rgba(255,_255,_255,_0.52)_99.99%,_rgba(0,_240,_255,_0))]" />
+    <div className="flex h-screen w-full items-center justify-center bg-mango-gray-light-2 bg-cover bg-center bg-no-repeat ">
+      <div className="flex min-h-[70%] w-[35%] flex-col items-center justify-between gap-2 rounded-2xl bg-white p-6 py-5">
+        {/* <div className="absolute left-[0px] top-[0px]  h-4 w-full rounded-t-xl bg-primary-states-hover" />
+        <div className="absolute left-[0px] top-[0px] h-4 w-60 rounded-t-xl [background:linear-gradient(90deg,_#80dfeb_68.23%,_rgba(255,_255,_255,_0.52)_99.99%,_rgba(0,_240,_255,_0))]" /> */}
         <div className=" my-12 text-center">
           <div className="flex items-center justify-center ">
             <img
@@ -34,43 +35,40 @@ const StoreWorkingHoursSetup: NextPage = () => {
             you
           </p>
         </div>
-        <div className="ml-8 mt-12 flex flex-col justify-center gap-[12px] text-text-primary">
+        <div className="w-[90%] flex flex-col justify-center gap-[12px] text-text-primary">
           {listData.map((item) => (
-            <div key={item.id} className="relative h-[38px] w-[289px]">
-              <div className="absolute left-[0px] top-[2px] h-[38px] w-[133px]">
-                <div className="absolute left-[0px] top-[2px] flex flex-row items-center justify-start">
-                  <div className="relative h-[38px] w-[58px]">
-                    <Switch className="absolute left-[0px] top-[5px] flex flex-row items-center justify-start p-3" />
+            <div key={item.id}>
+              <div>
+                <div className="flex flex-row items-center justify-start">
+                  <div>
+                    <Switch className="p-3" />
                   </div>
-                  <div className="relative hidden leading-[140%]">{`{Label}`}</div>
-                </div>
-                <div className="absolute left-[74px] top-[15px] font-semibold leading-[140%]">
-                  {item.day}
+                  <div className="font-semibold w-[70px] leading-[140%]">
+                    {item.day}
+                  </div>
+                  <div className="w-[70%] text-center leading-[140%]">
+                    {item.workTime}
+                  </div>
+                  {item.workTime !== "Closed" ? (
+                    <Image
+                      src="/chevronrightfilled.svg"
+                      width="20"
+                      height="20"
+                      alt=""
+                      objectFit="cover"
+                      className="hidden overflow-hidden"
+                    />
+                  ) : (
+                    ""
+                  )}
                 </div>
               </div>
-              <div className="absolute left-[236px] top-[15px] w-[70%] text-center leading-[140%]">
-                {item.workTime}
-              </div>
-              {item.workTime !== 'Closed' ? (
-                <img
-                  className="absolute left-[480px] top-[7px] h-6 w-6 overflow-hidden"
-                  alt=""
-                  src="/chevronrightfilled1.svg"
-                />
-              ) : (
-                ''
-              )}
-              <img
-                className="absolute left-[480px] top-[7px] hidden h-6 w-6 overflow-hidden"
-                alt=""
-                src="/chevronrightfilled.svg"
-              />
-              <div className="relative box-border h-px w-[505px] border-t-[1px] border-solid border-line-light p-[5px]" />
+              <div className="box-border h-px w-[100] border-t-[1px] border-solid border-line-light p-[5px]" />
             </div>
           ))}
           <button
             type="button"
-            className="mt-20 box-border flex w-[90%] flex-col items-center justify-center overflow-hidden rounded bg-primary-main px-[22px] py-[13px] text-[18px] font-bold text-primary-contrast shadow-[0px_1px_5px_rgba(0,_0,_0,_0.12),_0px_2px_2px_rgba(0,_0,_0,_0.14),_0px_3px_1px_-2px_rgba(0,_0,_0,_0.2)]"
+            className="mt-8 box-border flex w-[100%] flex-col items-center justify-center overflow-hidden rounded bg-primary-main px-[22px] py-[13px] text-[18px] font-bold text-primary-contrast shadow-[0px_1px_5px_rgba(0,_0,_0,_0.12),_0px_2px_2px_rgba(0,_0,_0,_0.14),_0px_3px_1px_-2px_rgba(0,_0,_0,_0.2)]"
           >
             CONTINUE
           </button>
