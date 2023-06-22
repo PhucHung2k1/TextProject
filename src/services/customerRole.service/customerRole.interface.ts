ce65df9f6ce65f5ace6214b4b40b5271b57d900f
tannd <124115539+TanEnrich@users.noreply.github.com>
2023-06-23 01:32:49 +0700

    Update customerRole.interface.ts

--- src/services/customerRole.service/customerRole.interface.ts
+++ src/services/customerRole.service/customerRole.interface.ts
@@ -1,67 +1,53 @@
 interface Employee {
-
   FirstName: string;
-
   LastName: string;
-
   Phone: string;
-
   Email: string;
-
   Status: any;
-
   ProfilePictureUrl: string;
-
   Id: string;
-
   CreateBy: any;
-
   CreateDate: Date;
-
   LastModifiedBy: any;
-
   LastModifiedDate: any;
-
 }
-
 interface PermissionItem {
-
   Name: string;
-
   SystemName: string;
-
   Category: string;
-
   Id: string;
-
   CreateBy: null;
-
   CreateDate: string;
-
   LastModifiedBy: null;
-
   LastModifiedDate: null;
-
+}
+export interface IAllCustomerRole {
+  Name: string;
+  Active: boolean;
+  IsSystemRole: boolean;
+  SystemName?: string;
+  Permissions: PermissionItem[];
+  Id: string;
+  StoreId?: string;
+  CreateDate?: Date;
+  Employees: Employee[];
 }
 
-export interface IAllCustomerRole {
-
+export interface IAddRemoveMultiRole {
+  AddedPermissions: string[];
+  RemovedPermissions: string[];
+}
+export interface IDetailRoleById {
   Name: string;
-
   Active: boolean;
-
   IsSystemRole: boolean;
-
-  SystemName?: string;
-
-  Permissions: PermissionItem[];
-
+  SystemName: null;
+  StoreId: string;
+  Type: null;
+  Permissions: any[];
   Id: string;
-
-  StoreId?: string;
-
-  CreateDate?: Date;
-
-  Employees: Employee[];
-
+  CreateBy: null;
+  CreateDate: Date | string;
+  LastModifiedBy: null;
+  LastModifiedDate: null;
 }
