8f79a9f413d6aafc8e579b4284ea37bca4a80ebb
PhucHung2k1 <phuchung.bd@enrichco.us>
2023-06-30 11:03:38 +0700

    Fix Select Store

--- src/pages/select-store/index.tsx
+++ src/pages/select-store/index.tsx
@@ -1,17 +1,10 @@
+import SelectStore from '@/components/Authentication/SelectStore';
 import React from 'react';
-import dynamic from 'next/dynamic';
 
-const DynamicComponent = dynamic(
-  () => import('@/components/Authentication/SelectStore'),
-  {
-    loading: () => <p>Loading...</p>, // Component hiển thị khi đang tải tệp tin
-    ssr: false, // Chạy dynamic import chỉ trên phía client
-  }
-);
 const SelectStorePage = () => {
   return (
     <div className="flex h-screen w-full items-center  justify-center bg-mango-gray-light-2  bg-cover bg-center bg-no-repeat ">
-      <DynamicComponent />
+      <SelectStore />
     </div>
   );
 };
