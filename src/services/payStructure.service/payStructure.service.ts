621e29f61332d272f9b51893d6e8eff77138659d
binhnt1509 <thanhbinh.bid@enrichco.us>
2023-06-30 16:49:51 +0700

    Delete Pay Structure
    Delete Pay Structure

--- src/services/payStructure.service/payStructure.service.ts
+++ src/services/payStructure.service/payStructure.service.ts
@@ -1,12 +1,18 @@
 import type { IResponse } from '@/utils/axios/entities';
 import { catchAxiosError } from '@/utils/axios/error';
-import { apiGet } from '@/utils/axios/instance';
+import { apiDelete, apiGet } from '@/utils/axios/instance';
 
 const GET_PAY_STRUCTURE = '/pay-structure';
-
 export class PayStructureService {
   public getPayStructure = async (): Promise<IResponse> => {
     const response: IResponse = await apiGet(GET_PAY_STRUCTURE).catch(
+      catchAxiosError
+    );
+    return response;
+  };
+
+  public deletePayStructure = async (id: string): Promise<IResponse> => {
+    const response: IResponse = await apiDelete(`${GET_PAY_STRUCTURE}/${id}`).catch(
       catchAxiosError
     );
     return response;
